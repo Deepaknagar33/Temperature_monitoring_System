790e7ba89ef7a712a1673cb5ef91e80d779028e2
Deepak Nagar <deepaknagarcsae@gmail.com>
2021-11-03 08:25:55 +0530

    intial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+"""Configurations for telegram_alert.py"""
+bolt_api_key = ""             # This is your Bolt Cloud API Key
+device_id = ""                # This is the device ID and will be similar to BOLTXXXX where XXXX is some numbers
+telegram_chat_id = ""         # This is the channel ID of the created Telegram channel. Paste after @ symbol.
+telegram_bot_id = ""          # This is the bot ID of the created Telegram Bot. Paste after bot text.
+threshold =                   # Threshold beyond which the alert should be sent
